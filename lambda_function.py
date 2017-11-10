398966963a1260717c445b56e7716816b6ff453d
Steve Winton <stevewinton@gmail.com>
2017-11-10 17:35:19 -0600

    Add base lambda_function

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+
+def lambda_handler(*args, **kwargs):
+    return "hello world"
+
+
+if __name__ == "__main__":
+    print lambda_handler(*sys.argv)
